f50fbc3a62242962625773d820db3cff3e0ccf18
michael seaman <mseaman26@gmail.com>
2024-01-26 13:22:34 -0800

    working nextAuth example from Brett Westwood

--- app/dashboard/page.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './dashboardPage.module.css'
-import UserInfo from '@/components/dashboard/userInfo'
-
-export default function DashboardPage(){
-    return(
-        <UserInfo/>
-    )
-}
--- middleware.js
+++
@@ -1,5 +0,0 @@
-//without a defined matcher, this one line applies next-auth to the entire project
-export { default } from 'next-auth/middleware'
-
-//using auth to protect specific pages
-export const config = {matcher: ['/poop']}
