50b5f4c018d8f9c8abb038b073837e70f73d4231
michael seaman <mseaman26@gmail.com>
2024-01-26 13:27:03 -0800

    capitalized User when importing User model

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
-import User from './components/user'
+import User from './components/User'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
