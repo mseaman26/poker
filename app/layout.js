cc52af79dc8a1b646ec76aa3b085666336c941aa
michael seaman <mseaman26@gmail.com>
2024-04-13 00:49:34 -0700

    test

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { AuthProvider } from "./Providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/nav/NavBar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta http-equiv="Accept-CH" content="DPR"/>
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
+      </Head>
       <body className={inter.className}>
         <AuthProvider>
           <NavBar/>
