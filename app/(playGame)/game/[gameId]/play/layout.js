1e2822f7ffbdf047790c7c0f9a88bcf109bf7981
michael seaman <mseaman26@gmail.com>
2024-06-03 15:29:10 -0700

    got rid of link that uses manifest

--- app/(playGame)/game/[gameId]/play/layout.js
+++ app/(playGame)/game/[gameId]/play/layout.js
@@ -10,7 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <link rel="manifest" href="manifest.json" />
       <body>
         <AuthProvider>
         {children}
