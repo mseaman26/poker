752b415eeb07f658d876f14da86cea4f04b6593f
michael seaman <mseaman26@gmail.com>
2024-07-30 13:02:15 -0700

    experiment with redirect to games instead

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -35,7 +35,7 @@ export default function LoginForm() {
           username: session.user.name,
           id: session.user.id
         })
-        router.push('/dashboard')
+        router.push('/games')
     }else if (status === 'unauthenticated'){
       setLoading(false)
       
